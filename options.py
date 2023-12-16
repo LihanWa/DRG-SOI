6134ec9581830c72fe955fd54d39cfd7e36f035b
Lihan Wang <105827673+LihanWa@users.noreply.github.com>
2023-12-16 16:13:50 +0800

    Update options.py

--- options.py
+++ options.py
@@ -6,7 +6,7 @@ parser.add_argument("--LongFormer", default='No', type=str, choices=['Yes','No']
 parser.add_argument("--use_AbbFull", default='No', type=str, choices=['Yes','No'], help="If you want to use my abbreviation-fullspelling.txt when creating data for training LongFormer, it should be set as Ture")
 parser.add_argument("--data_dir", default='data', type=str, help="Directory for data folder which contains root data, as well as subfolders for text files")
 parser.add_argument("--mimic_dir", default='~/mimic/', type=str, help="Dir for MIMIC-III or MIMIC-IV")
-parser.add_argument("--wd", default=1e-5, type=float)
+parser.add_argument("--wd", default=1e-4, type=float)
 parser.add_argument("--data_source", default='ms', type=str, choices=['ms', 'apr_mimic3','apr_mimic4'])
 parser.add_argument("--rule_dir", default='rules', type=str, help="path for officials `rules' for DRG grouping and weights")
 parser.add_argument("--dropout_rate", default=0.3, type=float)
